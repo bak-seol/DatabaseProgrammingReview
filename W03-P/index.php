a8cd63d51cb8fa7e39164391760b95ecf06d6d5e
park-seol <61292867+park-seol@users.noreply.github.com>
2020-09-21 20:06:06 +0900

    Add files via upload

---
+++ W03-P/index.php
@@ -0,0 +1,49 @@
+<?php
+   $link = mysqli_connect('localhost:3307', 'root', 'rootroot', 'dbp');
+   $query = "SELECT * FROM topic";
+   $result = mysqli_query($link, $query);
+   $list ='';
+   while ($row = mysqli_fetch_array($result)) {
+       $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+   }
+   $article = array(
+     'title' => 'Welcome',
+     'description' => ' PUPPY is cute... '
+   );
+  $update_link = '';
+  $delete_link = '';
+
+  if (isset($_GET['id'])) {
+      $query = " SELECT * FROM topic WHERE id = {$_GET['id']}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+      'title' => $row['title'],
+      'description'=> $row['description']
+    );
+      $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+      $delete_link = '
+      <form action ="process_delete.php" method = "POST">
+        <input type = "hidden" name = "id" value="'.$_GET['id'].'">
+        <input type ="submit" value="delete">
+      </form>
+    ';
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset = "utf-utf-8">
+  <title> PUPPY </title>
+</head>
+<body>
+  <h1> <a href="index.php"> PUPPY </a> </h1>
+  <ol><?= $list ?></ol>
+  <a href="create.php"> create </a>
+  <?= $update_link ?>
+  <?= $delete_link ?>
+  <h2> <?= $article['title']?> </h2>
+  <?= $article['description'] ?>
+</body>
+</html>
