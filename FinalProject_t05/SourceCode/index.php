b32f393bef551da0b6e1988b38eb48a77b6570b0
seol <61292867+bak-seol@users.noreply.github.com>
2020-12-14 11:08:48 +0900

    Create index.php

---
+++ FinalProject_t05/SourceCode/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title> 성북구 모범음식점 </title>
+</head>
+<body>
+    <h1> 성북구 모범음식점 </h1>
+    <a href="dongName.php">(1) 모범 음식점 지정 현황</a><br>
+    <a href="eachMenu.php">(2) 모범 음식점 업태명 분류</a><br>
+    <a href="selectedYear.php">(3) 전통있는(오래된) 모범음식점 현황 </a><br>
+    <a href="cancleStatus.php">(4) 모범 음식점 취소 현황</a><br>
+</body>
+</html>
